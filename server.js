b1cf466c4d6fcf4d921187bb4d3d595382f2b2d8
zoren rodelas <zorenrodelas30@gmail.com>
2026-03-13 12:51:49 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+//API framework
+
+const express = require('express');
+
+//cross origin resource sharing
+
+const cors=require('cors');
+
+//Environment variables
+
+require('dotenv').config();
+
+//Database connection
+
+const db=require('./config/db');
+
+//Routes
+
+const routes=require('./routes');
+
+//utilization of express
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+//Use routes
+
+app.use('/api',routes)
+
+//Server listening
+
+app.listen(process.env.PORT, () => {
+
+    console.log(`Server is running on port ${process.env.PORT}`);
+
+});
